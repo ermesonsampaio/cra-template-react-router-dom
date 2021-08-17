fd389f2d141bf4c6a8f890b7a6be37d0574dd451
ermesonsampaio <sampaioermesonjs@gmail.com>
2021-08-17 11:24:13 -0300

    Fix error

--- template/src/index.js
+++ template/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-import './App';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
